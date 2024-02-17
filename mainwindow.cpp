bbb92858e474ec06568185cb75e8c7ffb27ab729
qwint <svidinskyalexandr@gmail.com>
2024-02-17 17:17:44 +0200

    second commit task1lab1

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -16,17 +16,19 @@ MainWindow::~MainWindow()
 
 void MainWindow::paintEvent(QPaintEvent *)
 {
-
     QPainter painter;
     painter.begin(this);
 
-    QRectF rectangle(250.0, 220.0, 380.0, 220.0);
+    QRectF rectangle(250.0, 220.0, 380.0, 200.0);
     int startAngle = 180 * 16;
     int spanAngle = 180 * 16;
 
-    painter.setBrush(QBrush(Qt::red, Qt::SolidPattern));
+    painter.setBrush(QBrush(QColorConstants::Svg::brown, Qt::SolidPattern));
     painter.drawPie(rectangle, startAngle, spanAngle);
 
+    QPen pen(Qt::black, 10);
+    painter.setPen(pen);
+    painter.drawLine(440, 315, 440, 120);
 
     painter.end();
 }
